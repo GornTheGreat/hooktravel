c2884065ff1dceea1ad7d2a8a2e783b7cbddd207
nrodriguez <46190226q@institutmontilivi.cat>
2020-01-20 19:25:32 +0100

    Afegir bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <hello-world msg="puto"/>
   </div>
 </template>
 
@@ -12,6 +12,11 @@ export default {
   name: 'app',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      msg: String
+    }
   }
 }
 </script>
