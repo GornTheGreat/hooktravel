4e93484dd852983349a395fb5ba43a80d5df38c0
nrodriguez <46190226q@institutmontilivi.cat>
2020-01-21 16:15:04 +0100

    Component estructura de pagina principal

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    
-   
-
-    
+    <pagina-mapa></pagina-mapa>
   </div>
 </template>
 
 <script>
+import PaginaMapa from './components/PaginaMapa/PaginaMapa.vue'
 
 export default {
   name: 'app',
   components: {
+    PaginaMapa
   },
   data() {
    
@@ -20,12 +19,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/PaginaMapa/PaginaMapa.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="layout" class="container-fluid">
+        <div class="row no-gutters">
+            <div class="col-12">
+                <div class="mapa">
+                    MAPA
+                </div>
+            </div>
+        </div>
+        <div class="footer">
+            <div class="row">
+                <div class="col-4">
+                    <button>Llista</button>
+                </div>
+                <div class="col-4">
+                    <button>Mapa</button>
+                </div>
+                <div class="col-4">
+                    <button>Perfil</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script src="./PaginaMapa.js"></script>
+<style lang="scss" src="./PaginaMapa.scss"></style>
