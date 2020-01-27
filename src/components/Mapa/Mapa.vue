041ba0e903d8ac73ea801af416001e14c5d38d98
Pau Cuminal <45962238g@institutmontilivi.cat>
2020-01-27 18:49:28 +0100

    Afegir mapa google maps amb la api

--- src/components/Mapa/Mapa.vue
+++ src/components/Mapa/Mapa.vue
@@ -1,17 +1,48 @@
 <template>
 
 
-    <div id="mapa">
-        <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14113.511129159182!2d2.8253469446560233!3d41.9573301344991!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x74d05ccdc0610666!2sIES%20Montilivi!5e0!3m2!1ses!2ses!4v1579270883811!5m2!1ses!2ses"
-            width="100%" height="100%" frameborder="0" style="border:0;" allowfullscreen="">
-        </iframe>
-        <formlloc ></formlloc>
-        
-    </div>
+     <div id="map">
+
+<GmapMap
+  :center="{lat:10, lng:10}"
+  :zoom="7"
+  map-type-id="roadmap"
+  style="width: 100%; height: 100%"
+  
+  :options="{
+   zoomControl: true,
+   mapTypeControl: false,
+   scaleControl: true,
+   streetViewControl: false,
+   rotateControl: false,
+   fullscreenControl: false,
+   disableDefaultUi: false
+ }"
+ 
+>
+  <GmapMarker
+    :key="index"
+    v-for="(m, index) in markers"
+    :position="m.position"
+    :clickable="true"
+    :draggable="true"
+    @click="center=m.position"
+    
+  />
+</GmapMap>
+
+     </div>
 
 
 </template>
+
+
+    <!--Load the API from the specified URL
+    * The async attribute allows the browser to render the page while the API loads
+    * The key parameter will contain your own API key (which is not needed for this tutorial)
+    * The callback parameter executes the initMap() function
+    -->
+    
 
 <script src="./Mapa.js"></script>
 <style lang="scss" src="./Mapa.scss" scoped></style>
